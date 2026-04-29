30117fdd4dcff74fb93c2dcdd646bfd9ea11e8ce
massa <massanitatikawa@gmail.com>
2026-04-28 22:42:01 -0400

    Initial commit

---
+++ main/src/main/java/com/program/Main.java
@@ -0,0 +1,11 @@
+package com.program;
+
+import com.dominio.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "Carlos da silva", "CarlosSilva@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Davi silva", "DaviSilva@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Gustavo da silva", "GustavoSilva@gmail.com");
+    }
+}
